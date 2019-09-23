a799d0d501c3dd2b75e1db8ac405889939e5465c
Julian Elve <julian.elve@gmail.com>
2019-09-23 13:46:46 +0100

    change logging 2

--- src/functions/deploy-succeeded.js
+++ src/functions/deploy-succeeded.js
@@ -1,5 +1,7 @@
 export async function handler(event, context) {
-  console.log(event);
+  console.log('deploy-succeeded')
+  console.log('event', event)
+  console.log('context', context)
   return {
     statusCode: 200,
     body: JSON.stringify({ message: `Hello world` })
